7f25559fabcbf058857e4b526390bb9adf30cf39
code-env <org.codeen@gmail.com>
2024-11-23 17:10:07 +0100

    feat: fixing deployment error-> added the suspense

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,19 @@
 import MailContent from "@/components/mail-content";
 import MailHeader from "@/components/mail-header";
 import MailSidebar from "@/components/mail-sidebar";
-import React from "react";
+import React, { Suspense } from "react";
 
 const Page = () => {
   return (
-    <div className="flex flex-col gap-10">
-      <MailHeader />
-      <div className="flex gap-4">
-        <MailSidebar />
-        <MailContent />
+    <Suspense fallback={<div>Loading...</div>}>
+      <div className="flex flex-col gap-10">
+        <MailHeader />
+        <div className="flex gap-4">
+          <MailSidebar />
+          <MailContent />
+        </div>
       </div>
-    </div>
+    </Suspense>
   );
 };
 
